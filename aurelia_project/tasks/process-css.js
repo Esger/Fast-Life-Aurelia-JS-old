c87e43efe1b7e50d9de2ffb366bbab2893055535
Esger Jellema <esgerj@gmail.com>
2018-03-20 20:16:26 +0100

    process-css.js

--- aurelia_project/tasks/process-css.js
+++ aurelia_project/tasks/process-css.js
@@ -1,13 +1,9 @@
 import gulp from 'gulp';
-import changedInPlace from 'gulp-changed-in-place';
 import concat from 'gulp-concat';
 import sourcemaps from 'gulp-sourcemaps';
 import less from 'gulp-less';
 import plumber from 'gulp-plumber';
 import notify from 'gulp-notify';
-import postcss from 'gulp-postcss';
-import autoprefixer from 'autoprefixer';
-
 import project from '../aurelia.json';
 import { build } from 'aurelia-cli';
 
@@ -19,12 +15,8 @@ export default function processCSS() {
 
     return gulp.src(project.cssProcessor.source)
         .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
-        .pipe(changedInPlace({ firstPass: true }))
         .pipe(sourcemaps.init())
-        .pipe(postcss(processors))
         .pipe(concat('app.less'))
         .pipe(less())
         .pipe(gulp.dest('assets/'));
-    // .pipe(less().on('error', less.logError))
-    // .pipe(build.bundle());
-}
+}   
